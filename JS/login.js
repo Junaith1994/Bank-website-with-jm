204b07f96fb6bc88b3a05641d933528b34dfe2dc
Junaith1994 <daya2018ctg@gmail.com>
2022-07-01 15:30:29 +0600

    Added update in login page and css styles

--- JS/login.js
+++ JS/login.js
@@ -6,7 +6,7 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     // User Password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
-    if ((userEmail == "daya2015ctg@gmail.com") && (userPassword == "mikealpha")) {
+    if (((userEmail == "daya2015ctg@gmail.com") && (userPassword == "mikealpha")) || ((userEmail == "mdabdulwasay3@gmail.com") && (userPassword == "13121159")) || ((userEmail == "mizan@gmail.com") && (userPassword == "mizan2221"))) {
         window.location.href="bank-page.html";
     }
     else{console.log(alert("Invalid email or password"))}
